00789754951f2844da88aa0978b39fbe87a2cf3e
Paradox <Paradox@users.noreply.github.com>
2015-12-09 14:08:09 +0800

    fixes

--- pipu.py
+++
@@ -1,9 +0,0 @@
-import pip
-from subprocess import call
-
-import colorama
-colorama.init()
-
-for dist in pip.get_installed_distributions():
-    print(colorama.Fore.RED + 'Upgrading {0}'.format(dist.project_name) + colorama.Fore.BLACK)
-    call('pip install --upgrade {0} --user'.format(dist.project_name), shell=True)
